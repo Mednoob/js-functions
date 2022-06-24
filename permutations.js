ba3e01d234044a9ec654123496f9eb29c77b95d6
Ahmad Yaqdhan <ahmadyaqdhan123@gmail.com>
2022-06-24 17:13:17 +0900

    fix(permutations): forgot to add `s`

--- permutations.js
+++ permutations.js
@@ -4,7 +4,7 @@ function permutations(arr) {
     const res = [];
 
     for (let n = 0; n < arr.length; n++) {
-        const p = permutation(arr.slice(0, n).concat(arr.slice(n + 1, arr.length)));
+        const p = permutations(arr.slice(0, n).concat(arr.slice(n + 1, arr.length)));
         const ar = p.length ? p.map(x => [arr[n]].concat(x)) : [arr[n]];
         res.push(...ar);
     }
